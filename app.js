d76616511764980038cc0f8699df8ad39712d550
Leonardo <leonardobelilo@outlook.com>
2022-09-06 18:58:15 -0300

    branch final

--- app.js
+++ app.js
@@ -3,4 +3,5 @@ console.log("You can change the word")
 console.log("the things are happen")
 console.log("Greate job!")
 console.log("the things are happen here too")
+console.log("We ara work well!")
 
